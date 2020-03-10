8656d150edbc883bcb88b4e7bd189e626e9867ad
ericraider33 <9611418+ericraider33@users.noreply.github.com>
2020-03-10 15:29:13 -0400

    Tweaked example

--- pncs.cmd/examples/Altitude.cs
+++ pncs.cmd/examples/Altitude.cs
@@ -10,6 +10,27 @@ namespace pncs.cmd.examples
         {
             using (Pnyx p = new Pnyx())
             {
+                p.read("nya.csv");
+                p.parseCsv();
+                p.rowTransformerFunc(row =>
+                {
+                    var fullName = row[1];
+
+                    var name = pnyx.net.util.NameUtil.parseFullName(fullName);
+                    if (name == null)
+                        return null;
+
+                    return pnyx.net.util.RowUtil.replaceColumn(row, 2, name.firstName, name.lastName);
+                });
+                p.selectColumns(2,3,5);
+                p.columnTransformer(3, new pnyx.net.impl.DateTransform { formatSource = "M-d-yyyy", formatDestination = "yyyy-M-d"  });
+                p.lineTransformerFunc(x => pnyx.net.util.TextUtil.enocdeSqlValue(x));
+                p.print("insert into tmp_name values($1,$2,$3);");
+                p.write("nya.sql");
+            }
+
+            using (Pnyx p = new Pnyx())
+            {
                 p.read(@"C:\dev\asclepius\prod_import\alt.txt");
                 p.parseTab();
                 p.rowTransformerFunc(row =>
@@ -22,6 +43,7 @@ namespace pncs.cmd.examples
 
                     return pnyx.net.util.RowUtil.replaceColumn(row, 1, name.firstName, name.middleName, name.lastName);
                 });
+                p.lineTransformerFunc(x => pnyx.net.util.TextUtil.enocdeSqlValue(x));
                 p.sortRow(new[] {1, 3});
                 p.writeCsv(@"C:\dev\asclepius\prod_import\alt.csv");
             }
@@ -30,7 +52,7 @@ namespace pncs.cmd.examples
             {
                 p.read(@"C:\dev\asclepius\prod_import\alt_names.csv");
                 p.parseCsv();
-                p.withColumns(p2 => p2.lineTransformer(new DateTransform { formatSource = DateUtil.FORMAT_MDYYYY, formatDestination = DateUtil.FORMAT_ISO_8601_DATE  }), 3);
+                p.columnTransformer(3, new DateTransform { formatSource = DateUtil.FORMAT_MDYYYY, formatDestination = DateUtil.FORMAT_ISO_8601_DATE  });
                 p.rowTransformerFunc(row =>
                 {
                     for (int i = 0; i < row.Count; i++)
