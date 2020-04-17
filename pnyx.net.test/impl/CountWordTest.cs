57b8a8369a7c11b56f965e0ae014841b080f915e
ericraider33 <9611418+ericraider33@users.noreply.github.com>
2020-04-17 08:35:18 -0400

    Updated test case for count

---
+++ pnyx.net.test/impl/CountWordTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using pnyx.net.impl;
+using Xunit;
+
+namespace pnyx.net.test.impl
+{
+    public class CountWordTest
+    {
+        [Theory]
+        [InlineData(null, 0)]
+        [InlineData("", 0)]
+        [InlineData(" ", 0)]
+        [InlineData("a", 1)]
+        [InlineData(" a", 1)]
+        [InlineData("a ", 1)]
+        [InlineData("a1a", 1)]
+        [InlineData("a!a", 1)]
+        [InlineData("!aa", 1)]
+        [InlineData("a b", 2)]
+        [InlineData("a b c", 3)]
+        [InlineData("a @ ! 1", 4)]
+        public void countWords(String text, int expected)
+        {
+            Assert.Equal(expected, CountWords.countWords(text));
+        }
+    }
+}
