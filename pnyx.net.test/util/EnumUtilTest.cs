24cc87ddec0aa3f0b87ae4b5d6f38c236ae576f1
Eric Eschenbach <9611418+ericraider33@users.noreply.github.com>
2024-04-24 18:37:50 -0400

    Added a method to format an enum as text

--- pnyx.net.test/util/EnumUtilTest.cs
+++ pnyx.net.test/util/EnumUtilTest.cs
@@ -1,55 +1,69 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using pnyx.net.util;
 using Xunit;
 
-namespace pnyx.net.test.util
+namespace pnyx.net.test.util;
+
+public class EnumUtilTest
 {
-    public class EnumUtilTest
+    public enum GenderEnum
     {
-        public enum GenderEnum
-        {
-            Unspecified, Female, Male
-        }
+        Unspecified, Female, Male
+    }
 
-        private enum ToDictionaryTestEnum
-        {
-            Foo, Bar, Baz
-        }
+    private enum ToDictionaryTestEnum
+    {
+        Foo, Bar, Baz
+    }
 
-        [Fact]
-        public void stringToEnum()
-        {            
-            Assert.Equal(GenderEnum.Unspecified, EnumUtil.stringToEnum<GenderEnum>(null));
-            Assert.Equal(GenderEnum.Unspecified, EnumUtil.stringToEnum<GenderEnum>(""));
+    private enum LabelTestEnum
+    {
+        TreeHouse,
+        [Description("Google")]
+        ABC,
+    }
+
+    [Fact]
+    public void stringToEnum()
+    {            
+        Assert.Equal(GenderEnum.Unspecified, EnumUtil.stringToEnum<GenderEnum>(null));
+        Assert.Equal(GenderEnum.Unspecified, EnumUtil.stringToEnum<GenderEnum>(""));
             
-            Assert.Throws<ArgumentException>(() => EnumUtil.stringToEnum<GenderEnum>("x"));
+        Assert.Throws<ArgumentException>(() => EnumUtil.stringToEnum<GenderEnum>("x"));
             
-            Assert.Equal(GenderEnum.Male, EnumUtil.stringToEnum<GenderEnum>("male"));
-            Assert.Equal(GenderEnum.Male, EnumUtil.stringToEnum<GenderEnum>("MALE"));
-            Assert.Equal(GenderEnum.Male, EnumUtil.stringToEnum<GenderEnum>("Male"));
+        Assert.Equal(GenderEnum.Male, EnumUtil.stringToEnum<GenderEnum>("male"));
+        Assert.Equal(GenderEnum.Male, EnumUtil.stringToEnum<GenderEnum>("MALE"));
+        Assert.Equal(GenderEnum.Male, EnumUtil.stringToEnum<GenderEnum>("Male"));
             
-            Assert.Equal(GenderEnum.Female, EnumUtil.stringToEnum<GenderEnum>("female"));
-            Assert.Equal(GenderEnum.Female, EnumUtil.stringToEnum<GenderEnum>("FEMALE"));
-            Assert.Equal(GenderEnum.Female, EnumUtil.stringToEnum<GenderEnum>("Female"));            
-        }
+        Assert.Equal(GenderEnum.Female, EnumUtil.stringToEnum<GenderEnum>("female"));
+        Assert.Equal(GenderEnum.Female, EnumUtil.stringToEnum<GenderEnum>("FEMALE"));
+        Assert.Equal(GenderEnum.Female, EnumUtil.stringToEnum<GenderEnum>("Female"));            
+    }
 
-        [Fact]
-        public void testToDictionary()
+    [Fact]
+    public void testToDictionary()
+    {
+        Dictionary<string, ToDictionaryTestEnum> expected = new Dictionary<string, ToDictionaryTestEnum>
         {
-            Dictionary<string, ToDictionaryTestEnum> expected = new Dictionary<string, ToDictionaryTestEnum>
-            {
-                {Enum.GetName(typeof(ToDictionaryTestEnum), ToDictionaryTestEnum.Foo), ToDictionaryTestEnum.Foo},
-                {Enum.GetName(typeof(ToDictionaryTestEnum), ToDictionaryTestEnum.Bar), ToDictionaryTestEnum.Bar},
-                {Enum.GetName(typeof(ToDictionaryTestEnum), ToDictionaryTestEnum.Baz), ToDictionaryTestEnum.Baz}
-            };
+            {Enum.GetName(typeof(ToDictionaryTestEnum), ToDictionaryTestEnum.Foo), ToDictionaryTestEnum.Foo},
+            {Enum.GetName(typeof(ToDictionaryTestEnum), ToDictionaryTestEnum.Bar), ToDictionaryTestEnum.Bar},
+            {Enum.GetName(typeof(ToDictionaryTestEnum), ToDictionaryTestEnum.Baz), ToDictionaryTestEnum.Baz}
+        };
 
-            Dictionary<string, ToDictionaryTestEnum> actual = EnumUtil.toDictionary<ToDictionaryTestEnum>();
+        Dictionary<string, ToDictionaryTestEnum> actual = EnumUtil.toDictionary<ToDictionaryTestEnum>();
 
-            String x = String.Join(",", expected.OrderBy(kv => kv.Key).Select(kv => kv.Key + ":" + kv.Value));
-            String y = String.Join(",", actual.OrderBy(kv => kv.Key).Select(kv => kv.Key + ":" + kv.Value));
-            Assert.Equal(x, y);
-        }
+        String x = String.Join(",", expected.OrderBy(kv => kv.Key).Select(kv => kv.Key + ":" + kv.Value));
+        String y = String.Join(",", actual.OrderBy(kv => kv.Key).Select(kv => kv.Key + ":" + kv.Value));
+        Assert.Equal(x, y);
+    }
+
+    [Fact]
+    public void getLabel()
+    {
+        Assert.Equal("Tree House", LabelTestEnum.TreeHouse.getLabel());
+        Assert.Equal("Google", LabelTestEnum.ABC.getLabel());
     }
 }
--- pnyx.net/util/EnumUtil.cs
+++ pnyx.net/util/EnumUtil.cs
@@ -1,54 +1,76 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 
-namespace pnyx.net.util
+namespace pnyx.net.util;
+
+public static class EnumUtil
 {
-    public class EnumUtil
+    public static T stringToEnum<T>(String text, T defaultValue = default(T)) where T : struct
     {
-        public static T stringToEnum<T>(String text, T defaultValue = default(T)) where T : struct
-        {
-            if (String.IsNullOrEmpty(text))
-                return defaultValue;
+        if (String.IsNullOrEmpty(text))
+            return defaultValue;
 
-            return (T)Enum.Parse(typeof(T), text, true);
-        }
+        return (T)Enum.Parse(typeof(T), text, true);
+    }
 
-        public static T? stringToEnumNullable<T>(String text) where T : struct
-        {
-            if (String.IsNullOrEmpty(text) || TextUtil.isEqualsIgnoreCase(text, "null"))
-                return null;
+    public static T? stringToEnumNullable<T>(String text) where T : struct
+    {
+        if (String.IsNullOrEmpty(text) || TextUtil.isEqualsIgnoreCase(text, "null"))
+            return null;
 
-            return (T)Enum.Parse(typeof(T), text, true);
-        }
+        return (T)Enum.Parse(typeof(T), text, true);
+    }
 
-        public static List<T> valuesAsList<T>() where T : struct
-        {
-            T[] raw = (T[])Enum.GetValues(typeof(T));
-            return raw.ToList();
-        }
+    public static List<T> valuesAsList<T>() where T : struct
+    {
+        T[] raw = (T[])Enum.GetValues(typeof(T));
+        return raw.ToList();
+    }
 
-        public static List<T> valuesAsListIgnore<T>(params T[] toIgnore) where T : struct
-        {
-            T[] raw = (T[])Enum.GetValues(typeof(T));
-            List<T> result = raw.ToList();
+    public static List<T> valuesAsListIgnore<T>(params T[] toIgnore) where T : struct
+    {
+        T[] raw = (T[])Enum.GetValues(typeof(T));
+        List<T> result = raw.ToList();
 
-            foreach (T ignore in toIgnore)
-                result.Remove(ignore);
+        foreach (T ignore in toIgnore)
+            result.Remove(ignore);
             
-            return result;
-        }
+        return result;
+    }
 
-        public static Dictionary<String, T> toDictionary<T>(IEqualityComparer<String> comparer = null) where T : struct, IConvertible
-        {
-            comparer = comparer ?? StringComparer.OrdinalIgnoreCase;            
-            Dictionary<String, T> enumValues = new Dictionary<String, T>(comparer);
+    public static Dictionary<String, T> toDictionary<T>(IEqualityComparer<String> comparer = null) where T : struct, IConvertible
+    {
+        comparer = comparer ?? StringComparer.OrdinalIgnoreCase;            
+        Dictionary<String, T> enumValues = new Dictionary<String, T>(comparer);
             
-            Type enumType = typeof(T);
-            foreach (var value in Enum.GetValues(enumType))
-                enumValues.Add(Enum.GetName(enumType, value), (T) value);
+        Type enumType = typeof(T);
+        foreach (var value in Enum.GetValues(enumType))
+            enumValues.Add(Enum.GetName(enumType, value), (T) value);
 
-            return enumValues;
-        }        
+        return enumValues;
+    }
+
+    public static String getLabel(this Enum val)
+    {
+        if (val == null)
+            return null;
+        
+        Type type = val.GetType();
+        string valAsName = type.GetEnumName(val);
+        if (valAsName == null)
+            return null;
+            
+        MemberInfo mi = type.GetMember(valAsName).FirstOrDefault();
+        if (mi == null)
+            return null;
+
+        DescriptionAttribute da = mi.GetCustomAttributes<DescriptionAttribute>().FirstOrDefault();
+        if (da != null)
+            return da.Description;
+
+        return valAsName.camelToSpace();
     }
 }
