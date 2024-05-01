c74a869cb40a5e1bbf6ac5282628c00658065c9a
Eric Eschenbach <9611418+ericraider33@users.noreply.github.com>
2024-04-30 21:38:16 -0400

    Added formatPhone method

--- pnyx.net/util/PhoneUtil.cs
+++ pnyx.net/util/PhoneUtil.cs
@@ -1,24 +1,45 @@
 using System;
 
-namespace pnyx.net.util
+namespace pnyx.net.util;
+
+public static class PhoneUtil
 {
-    public static class PhoneUtil
+    public static String parsePhone(String phone, String defualtAreaCode = null)
     {
-        public static String parsePhone(String phone, String defualtAreaCode = null)
-        {
-            phone = TextUtil.emptyAsNull(ParseExtensions.extractNumeric(phone));
+        phone = ParseExtensions.extractNumeric(phone).emptyAsNull();
 
-            if (phone == null || phone.StartsWith("0"))
-                return null;
+        if (phone == null || phone.StartsWith("0"))
+            return null;
 
-            if (phone.StartsWith("1") && phone.Length >= 11)
-                phone = phone.Substring(1);
+        if (phone.StartsWith("1") && phone.Length >= 11)
+            phone = phone.Substring(1);
 
-            if (phone.Length == 7 && defualtAreaCode != null)
-                phone = defualtAreaCode + phone;
+        if (phone.Length == 7 && defualtAreaCode != null)
+            phone = defualtAreaCode + phone;
 
-            return phone.Length == 10 ? phone : null;
-        }        
-        
-    }
+        return phone.Length == 10 ? phone : null;
+    }        
+    
+    public static String formatPhone(String x)
+    {
+        x = x.trimEmptyAsNull();
+        if (x == null)
+            return null;
+
+        if (x.Length < 7)
+            return String.Concat("x", x);
+
+        if (x.Length < 10)
+            return x;
+
+        String areaCode = x.Substring(0, 3);
+        String switchCode = x.Substring(3, 3);
+        String exchangeCode = x.Substring(6, 4).Trim();
+        String result = $"({areaCode}) {switchCode}-{exchangeCode}";
+
+        if (x.Length > 10)
+            result = String.Join(" x", result, x.Substring(10));
+
+        return result;
+    }    
 }
