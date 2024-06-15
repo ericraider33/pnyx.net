1f7ea5cb86e7f9beab8faddedd93088f5c15eab9
Eric Eschenbach <9611418+ericraider33@users.noreply.github.com>
2024-06-15 17:40:16 -0400

    Verified phone util test cases with +1 format

--- pnyx.net.test/util/PhoneUtilTest.cs
+++ pnyx.net.test/util/PhoneUtilTest.cs
@@ -2,30 +2,30 @@ using System;
 using pnyx.net.util;
 using Xunit;
 
-namespace pnyx.net.test.util
-{
-    public class PhoneUtilTest
-    {
-        [Theory]
-        [InlineData(null, null, null)]
-        [InlineData("", null, null)]
-        [InlineData("asdfa", null, null)]
-        [InlineData("0123456789", null, null)]
+namespace pnyx.net.test.util;
 
-        // Wrong lengths
-        [InlineData("123456789", null, null)]
-        [InlineData("1234567", null, null)]
-        [InlineData("12345", null, null)]
+public class PhoneUtilTest
+{
+    [Theory]
+    [InlineData(null, null, null)]
+    [InlineData("", null, null)]
+    [InlineData("asdfa", null, null)]
+    [InlineData("0123456789", null, null)]
+
+    // Wrong lengths
+    [InlineData("123456789", null, null)]
+    [InlineData("1234567", null, null)]
+    [InlineData("12345", null, null)]
         
-        [InlineData("9995551234", null, "9995551234")]
-        [InlineData("5551234", "999", "9995551234")]
-        [InlineData("19995551234", null, "9995551234")]
-        [InlineData("15551234", "999", null)]
-        public void parsePhone(String phone, String defaultAreaCode, String expected)
-        {
-            String actual = PhoneUtil.parsePhone(phone, defaultAreaCode);
-            Assert.Equal(expected, actual);
-        }
-        
+    [InlineData("9995551234", null, "9995551234")]
+    [InlineData("5551234", "999", "9995551234")]
+    [InlineData("19995551234", null, "9995551234")]
+    [InlineData("+19995551234", null, "9995551234")]
+    [InlineData("15551234", "999", null)]
+    public void parsePhone(String phone, String defaultAreaCode, String expected)
+    {
+        String actual = PhoneUtil.parsePhone(phone, defaultAreaCode);
+        Assert.Equal(expected, actual);
     }
+        
 }
