8199660a8af9563adfc0a4d163106d8d1045a553
ericraider33 <9611418+ericraider33@users.noreply.github.com>
2026-06-25 11:12:26 -0400

    Coded first pass of CallImportService.matchCalls for matching staff and patients with calls

--- pnyx.net/util/CollectionUtil.cs
+++ pnyx.net/util/CollectionUtil.cs
@@ -1,10 +1,11 @@
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace pnyx.net.util;
 
 public static class CollectionUtil
 {
-    public static bool isEmpty(this ICollection? source)
+    public static bool isEmpty([NotNullWhen(false)] this ICollection? source)
     {
         return source == null || source.Count == 0;
     }
